e85509c25a8b7e2d15bb75164d0779ab0715532a
jiphendriks15 <120381948+jiphendriks15@users.noreply.github.com>
2023-03-24 00:19:52 +0100

    nieuwe ronde

--- game.js
+++ game.js
@@ -7,15 +7,34 @@ console.log ("waser")
 var plantengroei = document.querySelector(".kleinePlant");
 var waser = document.querySelector(".waser");
 var backgroundI = document.querySelector("body")
-var afbeeldingen = ['afbeelding/biksemplant.png', 'afbeelding/zonplant.png']
+var afbeeldingen = ['afbeelding/bliksum.png', 'afbeelding/super.png']
 subButton.addEventListener('click', getUserName, false); 
 var knop = document.querySelector('.knop');
 var eersteScherm = document.querySelector('#nameForm');
 var knup = document.querySelector(".knup");
 var welkom = document.querySelector(".welkom");
 var sun = document.querySelector(".sun");
-var moon = document.querySelector(".moon")
-var maantekst= document.querySelector(".maantekst")
+var moon = document.querySelector(".moon");
+var maantekst= document.querySelector(".maantekst");
+
+var probeer = document.querySelector(".probeer")
+
+var bijen = document.querySelector(".bijen");
+var bijgroei = document.querySelector(".kleinePlant");
+
+var maan = document.querySelector(".moon");
+var maangroei = document.querySelector(".kleinePlant");
+
+var zon = document.querySelector(".sun");
+var zongroei = document.querySelector(".kleinePlant");
+
+var water = document.querySelector(".waser")
+var watergroei = document.querySelector(".kleinePlant");
+
+var seed = document.querySelector(".seed");
+var zaadgroei = document.querySelector(".kleinePlant");
+
+
 
 knup.addEventListener("click", doeIetsWillekurigs);
 
@@ -39,6 +58,8 @@ else de tekst je plant heet en variable van start pagina verdwijnen. */
         welkom.classList.add('hide');
         sun.classList.remove('hide');
         moon.classList.remove('hide');
+        seed.classList.remove('hide');
+        bijen.classList.remove('hide');
     }
 }
 
@@ -64,26 +85,78 @@ function doeIetsWillekurigs(){
     plantengroei.src = afbeeldingen[index];
 }
 
-/* Wanneer er op een knop gedrukt wordt, laat er een gifje.*/
+/* Wanneer er op een knop gedrukt wordt, laat er een gifje.
 function groeiPlant(parameter){
     plantengroei.src = "afbeelding/begin.gif"
     console.log(plantengroei)
 };
 
+sun.addEventListener("click", groeiPlant);*/
+
+
+
+
+
+    
+  /* 
 waser.addEventListener("click", groeiPlant);
 
-
-
-sun.addEventListener("click", groeiPlant);
-
-
-moon.addEventListener('click', function(){
-    plantengroei.src = "afbeelding/klein.gif";
+*/
+moon.addEventListener('click', function maanGroei(){
+    maangroei.src = "afbeelding/start.png";
     maantekst.classList.remove('maanteksthide');
     setTimeout(function () {
         maantekst.classList.add('maanteksthide');
       }, 2000);
     });
+
+
+
+function zonGroei(parameter){
+    zongroei.src = "afbeelding/zon.png"
+    console.log(plantengroei)
+};
+
+sun.addEventListener("click", zonGroei);
+
+
+
+
+
+function waterGroei(parameter){
+    watergroei.src = "afbeelding/waterr.png"
+ 
+};
+
+waser.addEventListener("click", waterGroei);
+
+
+
+
+function zaadGroei(parameter){
+    zaadgroei.src = "afbeelding/zaadje.png"
+    console.log(zaadgroei)
+   /* probeer.classList.add("strike")*/
+};
+
+seed.addEventListener("click", zaadGroei);
+
+
+
+
+function bijGroei(parameter){
+    bijgroei.src = "afbeelding/bij.gif"
+    bijgroei.src = "afbeelding/vollebloem.png"
+    console.log(bijgroei)
+};
+
+bijen.addEventListener("click", bijGroei);
+
+
+
+
+    
+
     
 
 
